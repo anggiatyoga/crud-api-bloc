e9491c5d1ca63207748f94b8fd8ec0fd22d89740
anggiatyoga <anggiaty@gmail.com>
2020-04-30 21:12:10 +0700

    frist commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CRUD Bloc Demo',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.orange
       ),
